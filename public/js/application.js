746f46a9bae4230b748d314307da069b09e1fe4b
Jason Sutter <email": null>
2016-06-25 17:47:10 -0700

    created base google maps

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,31 @@
+function initMap() {
+      var myLatLng = {lat: 37.784701, lng: -122.397783};
+
+      var mapDiv = document.getElementById('map');
+      var map = new google.maps.Map(mapDiv, {
+          center: myLatLng,
+          zoom: 14
+      });
+
+    var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
