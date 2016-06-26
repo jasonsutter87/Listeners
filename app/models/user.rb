4b1e4d83a01fc7ed51ad26977d9fd4adabeeb3ca
Jason Sutter <jasonsutter87@gmail.com>
2016-06-26 00:31:31 -0700

    Working from home day 1

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :first_name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
+  has_many :locations
+
+  has_secure_password
+end
